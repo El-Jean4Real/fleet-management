0f8659b7a485a692876b4599a9210990964f4db4
El-Jean4Real <tinlin.tv@gmail.com>
2025-07-13 21:50:59 +0000

    update projet mise à jour 13-07-2025

--- application/logs/log-2025-07-13.php
+++ application/logs/log-2025-07-13.php
@@ -2773,3 +2773,38 @@ INFO - 2025-07-13 21:19:04 --> File loaded: /var/www/fleet/application/views/foo
 INFO - 2025-07-13 21:19:04 --> File loaded: /var/www/fleet/application/views/template.php
 INFO - 2025-07-13 21:19:04 --> Final output sent to browser
 DEBUG - 2025-07-13 21:19:04 --> Total execution time: 0.0093
+INFO - 2025-07-13 21:41:41 --> Config Class Initialized
+INFO - 2025-07-13 21:41:41 --> Hooks Class Initialized
+DEBUG - 2025-07-13 21:41:41 --> UTF-8 Support Enabled
+INFO - 2025-07-13 21:41:41 --> Utf8 Class Initialized
+INFO - 2025-07-13 21:41:41 --> URI Class Initialized
+DEBUG - 2025-07-13 21:41:41 --> No URI present. Default controller set.
+INFO - 2025-07-13 21:41:41 --> Router Class Initialized
+INFO - 2025-07-13 21:41:41 --> Output Class Initialized
+INFO - 2025-07-13 21:41:41 --> Security Class Initialized
+DEBUG - 2025-07-13 21:41:41 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-07-13 21:41:41 --> Input Class Initialized
+INFO - 2025-07-13 21:41:41 --> Language Class Initialized
+INFO - 2025-07-13 21:41:41 --> Loader Class Initialized
+INFO - 2025-07-13 21:41:41 --> Helper loaded: common_helper
+INFO - 2025-07-13 21:41:41 --> Helper loaded: url_helper
+INFO - 2025-07-13 21:41:41 --> Helper loaded: file_helper
+INFO - 2025-07-13 21:41:41 --> Database Driver Class Initialized
+DEBUG - 2025-07-13 21:41:41 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-07-13 21:41:41 --> Session: Class initialized using 'files' driver.
+DEBUG - 2025-07-13 21:41:41 --> Session class already loaded. Second attempt ignored.
+INFO - 2025-07-13 21:41:41 --> Controller Class Initialized
+INFO - 2025-07-13 21:41:41 --> Helper loaded: form_helper
+INFO - 2025-07-13 21:41:41 --> Helper loaded: language_helper
+DEBUG - 2025-07-13 21:41:41 --> Session class already loaded. Second attempt ignored.
+INFO - 2025-07-13 21:41:41 --> Form Validation Class Initialized
+DEBUG - 2025-07-13 21:41:41 --> Template class already loaded. Second attempt ignored.
+INFO - 2025-07-13 21:41:41 --> Language file loaded: language/english/main_lang.php
+INFO - 2025-07-13 21:41:41 --> Model "Customer_model" initialized
+INFO - 2025-07-13 21:41:41 --> Helper loaded: string_helper
+DEBUG - 2025-07-13 21:41:41 --> Form_validation class already loaded. Second attempt ignored.
+DEBUG - 2025-07-13 21:41:41 --> Session class already loaded. Second attempt ignored.
+INFO - 2025-07-13 21:41:41 --> Model "Trips_model" initialized
+INFO - 2025-07-13 21:41:41 --> File loaded: /var/www/fleet/application/views/frontend_booking.php
+INFO - 2025-07-13 21:41:41 --> Final output sent to browser
+DEBUG - 2025-07-13 21:41:41 --> Total execution time: 0.0053
